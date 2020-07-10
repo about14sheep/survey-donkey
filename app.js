287576bc837fb384ce8bc7a890d70f93c33a1c3b
Daniel Ramirez <danieldotcom2@gmail.com>
2020-07-10 16:49:31 -0500

    Add csurf protection and routes folder for routers

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const {Survey,Question,User} = require('./models');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const csurf = require('csurf');
+const surveyRouter = require('./routes/survey-router')
 
 const csrfProtection = csurf({cookie: true})
 const asyncHandler = (handler) => (req,res,next) => handler(req,res,next).catch(next);
 
+app.use(surveyRouter)
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: false}))
 app.set('view engine','pug')
