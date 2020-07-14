b1d39da6e6416d8c20d7e5945a8a496f4d659f0b
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-07-13 21:50:22 -0500

    create navbar

--- app.js
+++ app.js
@@ -11,18 +11,20 @@ const csrfProtection = csurf({ cookie: true })
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
 
+
 //app.use(surveyRouter)
 app.use(express.static('public'))
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'pug')
 
+
 // app.use(signUpRouter);-
 
 app.get('/', (req, res) => {
     res.render('splash')
 })
-
+a
 const port = Number.parseInt(process.env.PORT, 10) || 8081;
 app.listen(port, () => {
   console.log(`Listening for requests on port ${port}...`);
