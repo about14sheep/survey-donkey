004e1b9391c245fe18b6801e64b050b93062c6cf
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-07-15 13:13:08 -0500

    Correct Id Issue

--- seeders/20200710195412-InsertQuestionResponseData.js
+++ seeders/20200710195412-InsertQuestionResponseData.js
@@ -3,11 +3,11 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('QuestionResponses', [
-      { id: 1, surveyId: 1, userId: 3, questionId: 4, questionResponseValue: "option one", createdAt: new Date(), updatedAt: new Date() },
-      { id: 2, surveyId: 1, userId: 3, questionId: 4, questionResponseValue: "option one", createdAt: new Date(), updatedAt: new Date() },
-      { id: 3, surveyId: 1, userId: 2, questionId: 2, questionResponseValue: "option one", createdAt: new Date(), updatedAt: new Date() },
-      { id: 4, surveyId: 1, userId: 1, questionId: 2, questionResponseValue: "option one", createdAt: new Date(), updatedAt: new Date() },
-      { id: 5, surveyId: 1, userId: 4, questionId: 4, questionResponseValue: "option one", createdAt: new Date(), updatedAt: new Date() },
+      { surveyId: 1, userId: 3, questionId: 4, questionResponseValue: "option one", createdAt: new Date(), updatedAt: new Date() },
+      { surveyId: 1, userId: 3, questionId: 4, questionResponseValue: "option one", createdAt: new Date(), updatedAt: new Date() },
+      { surveyId: 1, userId: 2, questionId: 2, questionResponseValue: "option one", createdAt: new Date(), updatedAt: new Date() },
+      { surveyId: 1, userId: 1, questionId: 2, questionResponseValue: "option one", createdAt: new Date(), updatedAt: new Date() },
+      { surveyId: 1, userId: 4, questionId: 4, questionResponseValue: "option one", createdAt: new Date(), updatedAt: new Date() },
     ])
   },
 
