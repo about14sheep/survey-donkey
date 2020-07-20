a41809816805d1183f7cbb0fcb090279203d3c2c
Zackitty <59539209+Zackitty@users.noreply.github.com>
2020-07-19 20:23:25 -0500

    Update css and sorting capabilities

--- routes/feed.js
+++ routes/feed.js
@@ -112,4 +112,63 @@ router.get('/feed/modified', asyncHandler(async (req, res) => {
     })
 }))
 
+
+router.get('/feed/questions', asyncHandler(async (req, res) => {
+
+    const questionsFeedSurveys = await db.Survey.findAll({ include: [db.Question, db.User, db.Upvote]
+    });
+
+    function merge(leftArray, rightArray) {
+        const sorted = [];
+        while (leftArray.length > 0 && rightArray.length > 0) {
+          const leftItem = leftArray[0];
+          const rightItem = rightArray[0];
+
+          if (leftItem.Questions.length > rightItem.Questions.length) {
+            sorted.push(rightItem);
+            rightArray.shift();
+          } else {
+            sorted.push(leftItem);
+            leftArray.shift();
+          }
+        }
+
+        while (leftArray.length !== 0) {
+          const value = leftArray.shift();
+          sorted.push(value);
+        }
+
+        while (rightArray.length !== 0) {
+          const value = rightArray.shift();
+          sorted.push(value);
+        }
+
+        return sorted
+      }
+
+      function mergeSort(array) {
+        const length = array.length;
+
+        if (array.length < 2){
+          return array;
+        }
+
+        const middleIndex = Math.ceil(length / 2);
+        let leftArray = array.slice(0, middleIndex);
+        let rightArray = array.slice(middleIndex, length);
+
+        leftArray = mergeSort(leftArray);
+        rightArray = mergeSort(rightArray);
+
+        return merge(leftArray, rightArray);
+      }
+
+    let questionsSorted = mergeSort(questionsFeedSurveys).reverse();
+    res.render('feed', {
+        title: "SurveyDonkey Feed",
+        questionsSorted
+    })
+}))
+
+
 module.exports = router;
